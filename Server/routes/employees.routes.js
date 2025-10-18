22b5b705363514bd00f0769f49c36ad6a9a10d29
Gouranga <gourangasahoo712@gmail.com>
2025-10-18 13:55:06 +0530

    Item fetched

--- Server/routes/employees.routes.js
+++ Server/routes/employees.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { fetchAllEmployees,deleteEmployee } from '../controllers/employees.controller.js'
+import { fetchAllEmployees,deleteEmployee, getAllEmployeesGrouped, assignStaffToProject } from '../controllers/employees.controller.js'
 
 const router = express.Router()
 router.get('/fetchEmployees',fetchAllEmployees)
 router.delete('/deleteEmployee/:id',deleteEmployee)
+router.get("/grouped",getAllEmployeesGrouped);
+router.post("/assign",assignStaffToProject);
 
 export default router
